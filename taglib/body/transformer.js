4a9b2e3b7054df34ef3974e2f5495b480a8c0bf5
Austin Kelleher <austin.kell47@gmail.com>
2017-10-12 17:02:28 -0400

    Do not automatically inject browser-refresh into the body.

--- taglib/body/transformer.js
+++ taglib/body/transformer.js
@@ -1,6 +1,4 @@
 module.exports = function (el, context) {
-  var refresh = context.createNodeForEl('browser-refresh');
   var lasso = context.createNodeForEl('lasso-body');
-  el.appendChild(refresh);
   el.appendChild(lasso);
 };
